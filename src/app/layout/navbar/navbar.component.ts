2c0718c5c24c763c106c298a7ad79beb03154b0e
vivek <viveky055@gmail.com>
2024-03-24 20:21:14 +0530

    digitize

---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,53 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  animations: [
+    // trigger('slide_in_out', [
+    //   state('slide_in', style({
+    //     width: '350px',
+    //     // css styles when the element is in slide_in
+    //   })),
+    //   state('slide_out', style({
+    //     width: '0px'
+    //     // css styles when the element is in slide_out
+    //   })),
+    //   // animation effect when transitioning from one state to another
+    //   transition('slide_in <=> slide_out', animate('300ms ease'))
+    // ]),
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translateX(0)'
+      })),
+      transition(':enter', [
+        style({
+          transform: 'translateX(100%)'
+        }),
+        animate('300ms ease')
+      ]),
+      transition(':leave', [
+        animate('300ms ease', style({
+          transform: 'translateX(100%)'
+        }))
+      ])
+    ])
+  ]
+})
+export class NavbarComponent implements OnInit {
+  public isCollapsed = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  isMenuOpen = false;
+
+  toggleMenu() {
+
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}

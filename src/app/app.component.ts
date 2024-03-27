156a4eaaf728efe99e72fe74ad914409a0f2bec2
Shimray <ciaoshimray777@gmail.com>
2024-03-27 23:08:29 +0530

    swiper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { FooterComponent } from './layout/footer/footer.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app.routes';
-
+import { SwiperModule } from 'swiper/angular';
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [RouterOutlet,FooterComponent,NavbarComponent, HomeComponent],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent, HomeComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { SwiperModule } from 'swiper/angular';
+import SwiperCore, { SwiperOptions } from 'swiper';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SwiperModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  config: SwiperOptions = {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  };
+  onSwiper(swiper:any) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
 }

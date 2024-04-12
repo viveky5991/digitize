b0e99dd6be0722235ac79bab894963ceb9fe43d9
shimray <frontendkesari@gmail.com>
2024-04-12 10:03:13 +0530

    update design

--- src/app/stationary/stationarylisting/stationarylisting.component.ts
+++ src/app/stationary/stationarylisting/stationarylisting.component.ts
@@ -12,18 +12,18 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class StationarylistingComponent implements OnInit {
   title: string | undefined;
 
-  constructor( public _router: Router, private _route: ActivatedRoute,) { }
+  constructor(public _router: Router, private _route: ActivatedRoute,) { }
 
   ngOnInit(): void {
     this._route.url.subscribe((url: any) => {
-      debugger
-      if(url[0].path=='envelopes'){
-console.log('envelopes');
-this.title='Envelopes'
-      } else if(url[0].path=='business-cards'){
+      if (url[0].path == 'envelopes') {
+        console.log('envelopes');
+        this.title = 'Envelopes'
+      }
+      /* else if(url[0].path=='business-cards'){
         this.title='Business-Cards'
-      }else{
-        this.title='Digitize'
+      } */else {
+        this.title = 'Digitize'
       }
     })
   }

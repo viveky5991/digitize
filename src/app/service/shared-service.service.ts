57a0e489cff208adea4c43cd168a835f7e1c1f2a
shimray <frontendkesari@gmail.com>
2024-04-24 09:58:25 +0530

    service-data

--- src/app/service/shared-service.service.ts
+++ src/app/service/shared-service.service.ts
@@ -1,6 +1,7 @@
+// import { HttpClient } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,37 @@ export class SharedService {
 
   constructor(private http: HttpClient) { }
 
-  fetchData() {
-    // this.sharedData = data;
-    // return this.http.get<any>(`../../json/data.json`);
+  // fetchData() {
+  //   // this.sharedData = data;
+  //   return this.http.get<any>("/json/data.json") .pipe(
+  //     map((user: any) => {
+  //       // console.log(user);
+  //       return user;
+  //     })
+  //   )
+  // }
+  getData(): any[] {
+    return this.data;
   }
+  data = [
+    {
+      srcUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg',
+      previewUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg'
+    },
+    {
+      srcUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg',
+      previewUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg'
+    },
+    {
+      srcUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg',
+      previewUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg'
+    },
+    {
+      srcUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg',
+      previewUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg'
+    }
+  ];
+
   
 }
 

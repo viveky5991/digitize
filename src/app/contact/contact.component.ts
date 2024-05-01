d72153ca41bc1dc9d0059442c4e0165d8f6fd4b8
vivek <viveky055@gmail.com>
2024-05-02 00:03:33 +0530

    chag

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,21 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, NgForm, Validators,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-  formData: any = {};
+  contactForm: any;
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
 
-  submitForm() {
-    // Here you can handle the form submission logic, like sending data to a server
-    console.log(this.formData);
-    // Reset form after submission
-    this.formData = {};
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form submitted successfully!');
+      console.log(this.contactForm.value);
+      // You can add form submission logic here, such as sending the form data to a server
+      // Reset the form after submission
+      this.contactForm.reset();
+    } else {
+      console.log('Form is invalid, please check all fields.');
+    }
   }
 }
--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -2,16 +2,40 @@ import { Component } from '@angular/core';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 
+import { FormBuilder, FormGroup, FormsModule, NgForm, Validators,ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-popup',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule,FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.scss'
 })
 export class PopupComponent {
-  constructor(public dialog: MatDialog) {}
+  //constructor(public dialog: MatDialog) {}
+  contactForm: any;
+  constructor(private fb: FormBuilder ,public dialog: MatDialog) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      requirement: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
 
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form submitted successfully!');
+      console.log(this.contactForm.value);
+      // You can add form submission logic here, such as sending the form data to a server
+      // Reset the form after submission
+      this.contactForm.reset();
+    } else {
+      console.log('Form is invalid, please check all fields.');
+    }
+  }
  
 
 

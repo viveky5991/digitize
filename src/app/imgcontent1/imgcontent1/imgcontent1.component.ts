918ac89463265da5f81bdd1fd477d479a2abd4da
shimray <frontendkesari@gmail.com>
2024-05-06 09:48:15 +0530

    seal wax

--- src/app/imgcontent1/imgcontent1/imgcontent1.component.ts
+++ src/app/imgcontent1/imgcontent1/imgcontent1.component.ts
@@ -1,16 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-imgcontent1',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule,RouterModule,],
   templateUrl: './imgcontent1.component.html',
   styleUrl: './imgcontent1.component.scss'
 })
 export class Imgcontent1Component implements OnInit{
   title: string | undefined;
-  constructor( public _router: Router, private _route: ActivatedRoute,) { }
+  digitizedata: any;
+  constructor( public _router: Router, private _route: ActivatedRoute,private httpClient: HttpClient) { }
   ngOnInit(): void {
    
     this.navload()
@@ -19,11 +22,32 @@ export class Imgcontent1Component implements OnInit{
     this._route.url.subscribe((url: any) => {
       debugger
       if(url[0].path=='selfink-stamps'){
-        this.title='selfink-stamps'
+        this.title='Self Ink';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.selfink_stamps)
+          this.digitizedata = data.selfink_stamps;
+
+
+        })
       } else if(url[0].path=='wax-seal'){
-        this.title='wax-seal'
+        this.title='Wax-Seal';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.Wax_Seal)
+          this.digitizedata = data.Wax_Seal;
+
+
+        })
       }else if(url[0].path=='embossing-seal'){
-        this.title='embossing-seal'
+        this.title='Embossing-Seal';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.Embossing_Seal)
+          this.digitizedata = data.Embossing_Seal;
+
+
+        })
       }else if(url[0].path=='flyers'){
         this.title='flyers'
       }else if(url[0].path=='compliment-slips'){

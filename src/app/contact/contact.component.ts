3ea9fe43a7dc9325666ca0bb4d7fa487fc833eda
vivek <viveky055@gmail.com>
2024-05-12 23:54:47 +0530

    contact chg

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, NgForm, Validators,ReactiveFormsModule } from '@angular/forms';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule,CommonModule,ReactiveFormsModule],
+  imports: [FormsModule,CommonModule,ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -13,9 +14,10 @@ export class ContactComponent {
   contactForm: any;
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      phone: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required,Validators.pattern(/^[a-zA-Z .-]+$/)]],
+      phone: ['',  [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)]],
+      email: ['', [Validators.required,Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
+      address:[''],
       message: ['', Validators.required]
     });
   }

3ad2b24c2fd8c8a1439a582d0139ccd48d0f7244
vivek <viveky055@gmail.com>
2024-05-16 23:53:10 +0530

    about us

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+interface CounterItem {
+  count: number;
+  label: string;
+  target: number;
+  sign:string;
+}
 @Component({
   selector: 'app-aboutus',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './aboutus.component.html',
   styleUrl: './aboutus.component.scss'
 })
-export class AboutusComponent {
+export class AboutusComponent  implements OnInit{
+  // countItems: CounterItem[] = [
+  //   { count: 0, label: 'Reward', target: 50 },
+  //   { count: 0, label: 'Year+ Experience', target: 15 },
+  //   { count: 0, label: 'Projects Completed', target: 2040 },
+  //   { count: 0, label: 'Happy Clients', target: 1018 }
+  // ];
+  countItems: CounterItem[] = [
+    { count: 0, label: 'CUSTOMERS', target: 12000,sign:"+" },
+    { count: 0, label: 'TEAM MEMBERS', target: 130,sign:"+" },
+    { count: 0, label: 'SUPPORT AVAILABLE', target: 12,sign:"HR" },
+    { count: 0, label: 'CUP OF COFFEE', target: 265,sign:"+" },
+    { count: 0, label: 'POSITIVE REVIEWS', target: 99,sign:"%" }
+  ];
+  constructor(){}
+  ngOnInit(): void {
+    this.startCounters()
+  }
 
+  // startCounter(): void {
+  //   const countElements = document.querySelectorAll(".count");
+  //   const countArray = Array.from(countElements);
+
+  //   countArray.forEach(item => {
+  //     let startNumber = 0;
+  //     const targetNumber = parseInt(item.dataset.number || '0', 10); // Parse the dataset number as an integer
+
+  //     const counterUp = () => {
+  //       startNumber++;
+  //       item.innerHTML = startNumber.toString();
+
+  //       if (startNumber === targetNumber) {
+  //         clearInterval(stop);
+  //       }
+  //     };
+
+  //     const stop = setInterval(counterUp, 50);
+  //   });
+  // }
+  startCounters(): void {
+    this.countItems.forEach(item => {
+      let startNumber = 0;
+
+      const counterInterval = setInterval(() => {
+        startNumber++;
+        item.count = startNumber;
+
+        if (startNumber === item.target) {
+          clearInterval(counterInterval);
+        }
+      }, 50);
+    });
+  }
 }

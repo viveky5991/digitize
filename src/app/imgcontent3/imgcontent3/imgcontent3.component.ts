a1e392521b33d8ab604153196e1dc42780a8dc94
shimray <frontendkesari@gmail.com>
2024-05-22 10:29:57 +0530

    css upate

--- src/app/imgcontent3/imgcontent3/imgcontent3.component.ts
+++ src/app/imgcontent3/imgcontent3/imgcontent3.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { PopupComponent } from '../../popup/popup.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-imgcontent3',
@@ -13,7 +15,7 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 export class Imgcontent3Component  implements OnInit{
   title: string | undefined;
   digitizedata: any;
-  constructor( public _router: Router, private _route: ActivatedRoute,  private httpClient: HttpClient) { }
+  constructor( public _router: Router, private _route: ActivatedRoute,  private httpClient: HttpClient,public dialog: MatDialog) { }
   ngOnInit(): void {
 
     this.navload()
@@ -68,6 +70,28 @@ export class Imgcontent3Component  implements OnInit{
         this.title='Digitize'
       }
     })
+  }
+  EnquiryNow() {
+    // const dialogRef = this.dialog.open(PopupComponent);
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log(`Dialog result: ${result}`);
+    // });
+    const dialogRef = this.dialog.open(PopupComponent, {
+      width: '900px',
+    });
+    // dialogRef.afterOpened().subscribe(() => {
+    //   // Find the dialog container element by class name or any other means if necessary
+    //   const dialogContainer = document.querySelector('.mat-dialog-container');
+    //   // Check if the element exists
+    //   if (dialogContainer) {
+    //     // Set the role attribute to 'dialog'
+    //     this.renderer.setAttribute(dialogContainer, 'role', 'dialog');
+    //   }
+    // });
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('Dialog closed');
+    });
   }
 }
 

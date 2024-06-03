2c6754478a745e86e106f05b31e63f452fce5c1f
shimray <frontendkesari@gmail.com>
2024-06-03 09:52:36 +0530

    content9

--- src/app/imgcontent9/imgcontent9/imgcontent9.component.ts
+++ src/app/imgcontent9/imgcontent9/imgcontent9.component.ts
@@ -1,25 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { SwiperOptions } from 'swiper';
+import { Animations } from '../../animation';
+import { PopupComponent } from '../../popup/popup.component';
+import { CommonModule } from '@angular/common';
+import { SwiperModule } from 'swiper/angular';
 
 @Component({
   selector: 'app-imgcontent9',
   standalone: true,
-  imports: [],
+  imports: [SwiperModule, CommonModule, RouterModule,],
   templateUrl: './imgcontent9.component.html',
-  styleUrl: './imgcontent9.component.scss'
+  styleUrl: './imgcontent9.component.scss',
+  animations: Animations
 })
 export class Imgcontent9Component implements OnInit{
   title: string | undefined;
-  constructor( public _router: Router, private _route: ActivatedRoute,) { }
+  config: SwiperOptions = {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  };
+  digitizedata: any;
+  constructor( public _router: Router, private _route: ActivatedRoute,  private httpClient: HttpClient,public dialog: MatDialog) { }
   ngOnInit(): void {
-   
+
     this.navload()
   }
   navload(){
     this._route.url.subscribe((url: any) => {
       debugger
       if(url[0].path=='Pennant'){
-        this.title='Pennant'
+        this.title='Pennant Flags';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.Pennant)
+          this.digitizedata = data.Pennant;
+        })
       } else if(url[0].path=='Bunting'){
         this.title='Bunting'
       }else if(url[0].path=='Toothpick'){
@@ -38,6 +59,16 @@ export class Imgcontent9Component implements OnInit{
         this.title='Digitize'
       }
     })
+  }
+  EnquiryNow() {
+
+    const dialogRef = this.dialog.open(PopupComponent, {
+      width: '900px',
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('Dialog closed');
+    });
   }
 }
 

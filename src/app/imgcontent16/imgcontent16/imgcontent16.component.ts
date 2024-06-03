17429d331fec4e375bd2542ec0b31e7cb7be8b65
vivek <viveky055@gmail.com>
2024-06-03 23:51:49 +0530

    chg

--- src/app/imgcontent16/imgcontent16/imgcontent16.component.ts
+++ src/app/imgcontent16/imgcontent16/imgcontent16.component.ts
@@ -1,16 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { SwiperOptions } from 'swiper';
+import { PopupComponent } from '../../popup/popup.component';
+import { CommonModule } from '@angular/common';
+import { SwiperModule } from 'swiper/angular';
+import { Animations } from '../../animation';
 
 @Component({
   selector: 'app-imgcontent16',
   standalone: true,
-  imports: [],
+  imports: [SwiperModule,RouterModule,CommonModule],
   templateUrl: './imgcontent16.component.html',
-  styleUrl: './imgcontent16.component.scss'
+  styleUrl: './imgcontent16.component.scss',
+  animations: Animations
 })
 export class Imgcontent16Component  implements OnInit{
   title: string | undefined;
-  constructor( public _router: Router, private _route: ActivatedRoute,) { }
+  config: SwiperOptions = {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  };
+  digitizedata: any;
+  constructor( public _router: Router, private _route: ActivatedRoute,  private httpClient: HttpClient,public dialog: MatDialog) { }
   ngOnInit(): void {
    
     this.navload()
@@ -19,15 +35,25 @@ export class Imgcontent16Component  implements OnInit{
     this._route.url.subscribe((url: any) => {
       debugger
       if(url[0].path=='popups'){
-        this.title='popups'
+        this.title='popups';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.popups)
+          this.digitizedata = data.popups;
+        })
       } else if(url[0].path=='fabric-popups'){
-        this.title='fabric-popups'
+        this.title='fabric-popups';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.fabricpopups)
+          this.digitizedata = data.fabricpopups;
+        })
       }else if(url[0].path=='rollup-banners'){
         this.title='rollup-banners'
       }else if(url[0].path=='x-banners'){
-        this.title='x-banners'
+        this.title='x-banners';
       }else if(url[0].path=='banners-flex'){
-        this.title='banners-flex'
+        this.title='banners-flex';
       }else if(url[0].path=='fence-banners'){
         this.title='fence-banners'
       }else if(url[0].path=='lama-stand'){
@@ -37,5 +63,16 @@ export class Imgcontent16Component  implements OnInit{
       }
     })
   }
+  EnquiryNow() {
+
+    const dialogRef = this.dialog.open(PopupComponent, {
+      width: '900px',
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('Dialog closed');
+    });
+  }
+
 }
 
--- src/app/imgcontent18/imgcontent18/imgcontent18.component.ts
+++ src/app/imgcontent18/imgcontent18/imgcontent18.component.ts
@@ -1,16 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { SwiperOptions } from 'swiper';
+import { PopupComponent } from '../../popup/popup.component';
+import { CommonModule } from '@angular/common';
+import { SwiperModule } from 'swiper/angular';
+import { Animations } from '../../animation';
 
 @Component({
   selector: 'app-imgcontent18',
   standalone: true,
-  imports: [],
+  imports: [SwiperModule,RouterModule,CommonModule],
   templateUrl: './imgcontent18.component.html',
-  styleUrl: './imgcontent18.component.scss'
+  styleUrl: './imgcontent18.component.scss',
+  animations: Animations
 })
 export class Imgcontent18Component implements OnInit{
   title: string | undefined;
-  constructor( public _router: Router, private _route: ActivatedRoute,) { }
+  config: SwiperOptions = {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  };
+  digitizedata: any;
+  constructor( public _router: Router, private _route: ActivatedRoute,  private httpClient: HttpClient,public dialog: MatDialog) { }
   ngOnInit(): void {
    
     this.navload()
@@ -19,13 +35,33 @@ export class Imgcontent18Component implements OnInit{
     this._route.url.subscribe((url: any) => {
       debugger
       if(url[0].path=='promotion'){
-        this.title='promotion'
+        this.title='promotion';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.promotion)
+          this.digitizedata = data.promotion;
+        })
       } else if(url[0].path=='exhibition-counter'){
-        this.title='exhibition-counter'
+        this.title='exhibition-counter';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.exhibitioncounter)
+          this.digitizedata = data.exhibitioncounter;
+        })
       } else if(url[0].path=='tent-canopy'){
-        this.title='tent-canopy'
+        this.title='tent-canopy';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.tentcanopy)
+          this.digitizedata = data.tentcanopy;
+        })
       } else if(url[0].path=='parasol-umbrella'){
-        this.title='parasol-umbrella'
+        this.title='parasol-umbrella';
+        this.httpClient.get<any>("assets/data.json").subscribe((data) => {
+
+          console.log(data.parasolumbrella)
+          this.digitizedata = data.parasolumbrella;
+        })
       } else if(url[0].path=='cover-cloth'){
         this.title='cover-cloth'
       } else if(url[0].path=='media-frame'){
@@ -40,6 +76,16 @@ export class Imgcontent18Component implements OnInit{
         this.title='Digitize'
       }
     })
+  }
+  EnquiryNow() {
+
+    const dialogRef = this.dialog.open(PopupComponent, {
+      width: '900px',
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('Dialog closed');
+    });
   }
 }
 

3e1cc34daba90290635e218731bcb8ed472a07c0
shimray <frontendkesari@gmail.com>
2024-06-12 10:04:11 +0530

    responsive update

--- src/app/imgcontent4/imgcontent4/imgcontent4.component.ts
+++ src/app/imgcontent4/imgcontent4/imgcontent4.component.ts
@@ -11,72 +11,82 @@ import { Animations } from '../../animation';
 @Component({
   selector: 'app-imgcontent4',
   standalone: true,
-  imports: [SwiperModule,RouterModule,CommonModule],
+  imports: [SwiperModule, RouterModule, CommonModule],
   templateUrl: './imgcontent4.component.html',
   styleUrl: './imgcontent4.component.scss',
   animations: Animations
 })
-export class Imgcontent4Component  implements OnInit{
+export class Imgcontent4Component implements OnInit {
   title: string | undefined;
   config: SwiperOptions = {
-    slidesPerView: 2,
-    spaceBetween: 50,
+    loop: false,
+    allowTouchMove: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      clickable: true
+    },
     navigation: true,
-    pagination: { clickable: true },
-    scrollbar: { draggable: true },
+    slidesPerView: 1,
+    spaceBetween: 50,
+    scrollbar: {
+      draggable: true
+    }
   };
   digitizedata: any;
-  constructor( public _router: Router, private _route: ActivatedRoute,  private httpClient: HttpClient,public dialog: MatDialog) { }
+  constructor(public _router: Router, private _route: ActivatedRoute, private httpClient: HttpClient, public dialog: MatDialog) { }
   ngOnInit(): void {
 
     this.navload()
   }
-  navload(){
+  navload() {
     this._route.url.subscribe((url: any) => {
-      if(url[0].path=='Unlit3D'){
-        this.title='Unlit 3D';
+      if (url[0].path == 'Unlit3D') {
+        this.title = 'Unlit 3D';
         this.httpClient.get<any>("assets/data.json").subscribe((data) => {
 
           console.log(data.Unlit3D)
           this.digitizedata = data.Unlit3D;
         })
-      } else if(url[0].path=='Frontlit3D'){
-        this.title='Frontlit 3D';
+      } else if (url[0].path == 'Frontlit3D') {
+        this.title = 'Frontlit 3D';
         this.httpClient.get<any>("assets/data.json").subscribe((data) => {
 
           console.log(data.Frontlit3D)
           this.digitizedata = data.Frontlit3D;
         })
-      }else if(url[0].path=='Backlit3D'){
-        this.title='Backlit 3D';
+      } else if (url[0].path == 'Backlit3D') {
+        this.title = 'Backlit 3D';
         this.httpClient.get<any>("assets/data.json").subscribe((data) => {
 
           console.log(data.Backlit3D)
           this.digitizedata = data.Backlit3D;
         })
-      }else if(url[0].path=='Outlit3D'){
-        this.title='Outlit 3D';
+      } else if (url[0].path == 'Outlit3D') {
+        this.title = 'Outlit 3D';
         this.httpClient.get<any>("assets/data.json").subscribe((data) => {
 
           console.log(data.Outlit3D)
           this.digitizedata = data.Outlit3D;
         })
-      }else if(url[0].path=='PushThrough3D'){
-        this.title='Push Through 3D';
+      } else if (url[0].path == 'PushThrough3D') {
+        this.title = 'Push Through 3D';
         this.httpClient.get<any>("assets/data.json").subscribe((data) => {
 
           console.log(data.PushThrough3D)
           this.digitizedata = data.PushThrough3D;
         })
-      }else if(url[0].path=='Neon3D'){
-        this.title='Neon 3D';
+      } else if (url[0].path == 'Neon3D') {
+        this.title = 'Neon 3D';
         this.httpClient.get<any>("assets/data.json").subscribe((data) => {
 
           console.log(data.Neon3D)
           this.digitizedata = data.Neon3D;
         })
-      }else{
-        this.title='Digitize'
+      } else {
+        this.title = 'Digitize'
       }
     })
   }
